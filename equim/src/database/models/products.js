d9c83bf0d982747809ec18ec20fec812824a87ea
Diego1281 <chavo73_casm@hotmail.com>
2020-10-09 21:59:55 -0300

    sube carpeta database archivo mysql en carpeta data y modelo de productos

---
+++ equim/src/database/models/products.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Products";
+    let cols = {
+        id:{
+            type: dataTypes.INTEGER(11),
+            allowNull:false,
+            autoIncrement:true,
+            primaryKey:true
+        },
+        nombre: {
+            type:dataTypes.STRING(45),
+            allowNull:false
+        },
+        precio: {
+            type:dataTypes.INTEGER(11),
+            allowNull:false
+        },
+        
+        descripcion:{
+            type:dataTypes.STRING(300),
+            allowNull:false
+        },
+        imagenes:{
+            type:dataTypes.STRING(100),
+            allowNull:false
+        }
+    }
+
+    let config = {
+        tableName  : "products",
+        timestamps: true,
+        underscored: true
+    }
+    const Product = sequelize.define(alias,cols,config)
+
+    return Product;
+}
